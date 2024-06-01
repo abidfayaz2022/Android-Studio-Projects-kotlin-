231a549182926f824be49e6bc39239c6206ae769
Your Name <you@example.com>
2024-06-02 00:43:47 +0530

    MVVM model added and completed

---
+++ CapainGame/app/src/main/java/com/example/capaingame/CounterViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.capaingame
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+
+open class CounterViewModel : ViewModel() {
+
+}
+
+}
---
+++ CounterMVVM/app/src/main/java/com/example/countermvvm/MainActivity.kt
@@ -0,0 +1,63 @@
+
+package com.example.countermvvm
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.countermvvm.ui.theme.CounterMVVMTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            val viewModel:CounterMVVM = viewModel()
+            CounterMVVMTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    TheCounterApp(viewModel)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun TheCounterApp(viewModel: CounterMVVM){
+Column (modifier =Modifier.fillMaxSize(),
+    verticalArrangement = Arrangement.Center,
+    horizontalAlignment = Alignment.CenterHorizontally){
+
+    Text(text ="Count: ${viewModel.count.value}",
+        fontSize = 24.sp,
+        fontWeight = FontWeight.Bold
+    )
+    Row {
+        Button(onClick = { viewModel.increment() }) {
+            Text(text = "Increment")
+        }
+        Button(onClick = {viewModel.decrement()}) {
+            Text(text = "Decrement")
+        }
+    }
+
+}
+}
